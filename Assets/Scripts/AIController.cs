15f95df7b7fe5a513903fa6dc81560ac66169f55
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2023-08-17 22:16:23 -0700

    Import Easy Road Assets, move car and circuits to test scene, create capsule tracker for car to follow

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -14,6 +14,10 @@ public class AIController : MonoBehaviour
     int currentWP = 0;
     float totalDistanceToTarget;
 
+    GameObject tracker;
+    int currentTrackerWP = 0;
+    float lookAhead = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,32 +25,64 @@ public class AIController : MonoBehaviour
         target = circuit.waypoints[currentWP].transform.position;
         nextTarget = circuit.waypoints[currentWP + 1].transform.position;
         totalDistanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
+
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.transform.position = drive.rb.gameObject.transform.position;
+        tracker.transform.rotation = drive.rb.gameObject.transform.rotation;
+    }
+
+    /// <summary>
+    /// Moves tracker around track
+    /// </summary>
+    void ProgressTracker()
+    {
+        Debug.DrawLine(drive.rb.gameObject.transform.position, tracker.transform.position);
+
+        if (Vector3.Distance(drive.rb.gameObject.transform.position, tracker.transform.position) > lookAhead)
+        {
+            return;
+        }
+
+        tracker.transform.LookAt(circuit.waypoints[currentTrackerWP].transform.position);
+        tracker.transform.Translate(0, 0, 1.0f); //speed of tracker
+
+        if (Vector3.Distance(tracker.transform.position, circuit.waypoints[currentTrackerWP].transform.position) < 1)
+        {
+            currentTrackerWP++;
+            if (currentTrackerWP >= circuit.waypoints.Length)
+            {
+                currentTrackerWP = 0;
+            }
+        }
     }
 
     // Update is called once per frame
     bool isJumping = false;
     void Update()
     {
+        ProgressTracker();
         // translates the target's coordinates into the space of the vehicle
         // the vehicle's rigid body becomes the origin and assigns it to the localTarget
-        Vector3 localTarget = drive.rb.gameObject.transform.InverseTransformPoint(target);
-        Vector3 nextLocalTarget = drive.rb.gameObject.transform.InverseTransformPoint(nextTarget);
-        float distanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
+        Vector3 localTarget = drive.rb.gameObject.transform.InverseTransformPoint(tracker.transform.position);
+        //Vector3 nextLocalTarget = drive.rb.gameObject.transform.InverseTransformPoint(nextTarget);
+        //float distanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
 
         float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-        float nextTargetAngle = Mathf.Atan2(nextLocalTarget.x, nextLocalTarget.z) * Mathf.Rad2Deg;
+        //float nextTargetAngle = Mathf.Atan2(nextLocalTarget.x, nextLocalTarget.z) * Mathf.Rad2Deg;
 
         float steer = Mathf.Clamp(targetAngle * steeringSensitivity, - 1, 1) * Mathf.Sign(drive.currentSpeed);
+        float brake = 0;
+        float accel = 0.5f;
+        //float distanceFactor = distanceToTarget / totalDistanceToTarget;
+        //float speedFactor = drive.currentSpeed / drive.maxSpeed;
 
-        float distanceFactor = distanceToTarget / totalDistanceToTarget;
-        float speedFactor = drive.currentSpeed / drive.maxSpeed;
-
-        float accel = Mathf.Lerp(accelSensitivity, 1, distanceFactor);
+        //float accel = Mathf.Lerp(accelSensitivity, 1, distanceFactor);
 
         // when distance factor is 0, car has no braking
         // when distance factor is 1, cas has full braking
         // if distance factor is between 0 and 1, it will be somewhere between 
-        float brake = Mathf.Lerp((-1 - Mathf.Abs(nextTargetAngle)) * brakingSensitivity, 1 + speedFactor, 1 - distanceFactor);
+        //float brake = Mathf.Lerp((-1 - Mathf.Abs(nextTargetAngle)) * brakingSensitivity, 1 + speedFactor, 1 - distanceFactor);
 
         //if (distanceToTarget < 8 && Mathf.Abs(nextTargetAngle) > 20)
         //{
@@ -54,12 +90,12 @@ public class AIController : MonoBehaviour
         //    accel -= 0.8f;
         //}
 
-        if (isJumping)
-        {
-            accel = 1;
-            brake = 0;
-            Debug.Log("Jumping");
-        }
+        //if (isJumping)
+        //{
+        //    accel = 1;
+        //    brake = 0;
+        //    Debug.Log("Jumping");
+        //}
 
         Debug.Log("Brake" + brake + ", Accel: " + accel + ", Speed: " + drive.rb.velocity.magnitude + ", Time: " + Mathf.Round(Time.time) + " seconds");
 
@@ -73,24 +109,24 @@ public class AIController : MonoBehaviour
 
         // have a small threshold
         // make larger if car starts to circle waypoint
-        if (distanceToTarget < 4)
-        {
-            currentWP++;
-            if (currentWP >= circuit.waypoints.Length)
-                currentWP = 0;
-            target = circuit.waypoints[currentWP].transform.position;
-            if (currentWP == circuit.waypoints.Length - 1)
-                nextTarget = circuit.waypoints[0].transform.position;
-            else
-                nextTarget = circuit.waypoints[currentWP + 1].transform.position;
-            totalDistanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
+        //if (distanceToTarget < 4)
+        //{
+        //    currentWP++;
+        //    if (currentWP >= circuit.waypoints.Length)
+        //        currentWP = 0;
+        //    target = circuit.waypoints[currentWP].transform.position;
+        //    if (currentWP == circuit.waypoints.Length - 1)
+        //        nextTarget = circuit.waypoints[0].transform.position;
+        //    else
+        //        nextTarget = circuit.waypoints[currentWP + 1].transform.position;
+        //    totalDistanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
 
-            if (drive.rb.gameObject.transform.InverseTransformPoint(target).y > 5)
-            {
-                isJumping = true;
-            }
-            else isJumping = false;
-        }
+        //    if (drive.rb.gameObject.transform.InverseTransformPoint(target).y > 5)
+        //    {
+        //        isJumping = true;
+        //    }
+        //    else isJumping = false;
+        //}
 
         drive.CheckForSkid();
         drive.CalculateEngineSound();
