8950f6d11d75d965143806eb4a97d26ee0164ab0
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2023-08-17 23:05:20 -0700

    cleaning code, adjust car size, lerp acceleration and break factors

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -16,7 +16,7 @@ public class AIController : MonoBehaviour
 
     GameObject tracker;
     int currentTrackerWP = 0;
-    float lookAhead = 10;
+    public float lookAhead = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -65,68 +65,28 @@ public class AIController : MonoBehaviour
         // translates the target's coordinates into the space of the vehicle
         // the vehicle's rigid body becomes the origin and assigns it to the localTarget
         Vector3 localTarget = drive.rb.gameObject.transform.InverseTransformPoint(tracker.transform.position);
-        //Vector3 nextLocalTarget = drive.rb.gameObject.transform.InverseTransformPoint(nextTarget);
-        //float distanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
-
         float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-        //float nextTargetAngle = Mathf.Atan2(nextLocalTarget.x, nextLocalTarget.z) * Mathf.Rad2Deg;
-
         float steer = Mathf.Clamp(targetAngle * steeringSensitivity, - 1, 1) * Mathf.Sign(drive.currentSpeed);
+        float speedFactor = drive.currentSpeed / drive.maxSpeed;
+
+        float corner = Mathf.Clamp(Mathf.Abs(targetAngle), 0, 90);
+        float cornerFactor = corner / 90.0f;
+
         float brake = 0;
-        float accel = 0.5f;
-        //float distanceFactor = distanceToTarget / totalDistanceToTarget;
-        //float speedFactor = drive.currentSpeed / drive.maxSpeed;
+        if (corner > 10 && speedFactor > 0.1f)
+        {
+            brake = Mathf.Lerp(0, 1 + speedFactor * brakingSensitivity, cornerFactor);
+        }
 
-        //float accel = Mathf.Lerp(accelSensitivity, 1, distanceFactor);
-
-        // when distance factor is 0, car has no braking
-        // when distance factor is 1, cas has full braking
-        // if distance factor is between 0 and 1, it will be somewhere between 
-        //float brake = Mathf.Lerp((-1 - Mathf.Abs(nextTargetAngle)) * brakingSensitivity, 1 + speedFactor, 1 - distanceFactor);
-
-        //if (distanceToTarget < 8 && Mathf.Abs(nextTargetAngle) > 20)
-        //{
-        //    brake += 0.8f;
-        //    accel -= 0.8f;
-        //}
-
-        //if (isJumping)
-        //{
-        //    accel = 1;
-        //    brake = 0;
-        //    Debug.Log("Jumping");
-        //}
+        float accel = 1f;
+        if (corner > 20 && speedFactor > 0.2f)
+        {
+            accel = Mathf.Lerp(0, 1 * accelSensitivity, 1 - cornerFactor);
+        }
 
         Debug.Log("Brake" + brake + ", Accel: " + accel + ", Speed: " + drive.rb.velocity.magnitude + ", Time: " + Mathf.Round(Time.time) + " seconds");
 
-        //if (distanceToTarget < 5)
-        //{
-        //    brake = 0.8f; accel = 0.1f;
-        //}
-
-        // the vehicle game object moves drives on its own
         drive.Go(accel, steer, brake);
-
-        // have a small threshold
-        // make larger if car starts to circle waypoint
-        //if (distanceToTarget < 4)
-        //{
-        //    currentWP++;
-        //    if (currentWP >= circuit.waypoints.Length)
-        //        currentWP = 0;
-        //    target = circuit.waypoints[currentWP].transform.position;
-        //    if (currentWP == circuit.waypoints.Length - 1)
-        //        nextTarget = circuit.waypoints[0].transform.position;
-        //    else
-        //        nextTarget = circuit.waypoints[currentWP + 1].transform.position;
-        //    totalDistanceToTarget = Vector3.Distance(target, drive.rb.gameObject.transform.position);
-
-        //    if (drive.rb.gameObject.transform.InverseTransformPoint(target).y > 5)
-        //    {
-        //        isJumping = true;
-        //    }
-        //    else isJumping = false;
-        //}
 
         drive.CheckForSkid();
         drive.CalculateEngineSound();
