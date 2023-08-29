2b6000709e53c911d00689ecea833a69b0055971
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2023-08-28 18:18:56 -0700

    Create MiniMap Camera with temp img rendered for player car rendered on map

---
+++ Assets/HUDController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+    float HUDSetting = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = this.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
