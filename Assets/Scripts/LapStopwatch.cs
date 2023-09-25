5b5976808f70245ed73c9338f8fd50e2bc3978b3
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2023-09-24 21:07:04 -0700

    Create stopwatch, adjust minimap render texture, set scoreCount (if using)

---
+++ Assets/Scripts/LapStopwatch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+//https://www.youtube.com/watch?v=HLz_k6DSQvU
+public class LapStopwatch : MonoBehaviour
+{
+    bool stopwatchActive = false;
+    float currentTime;
+    public TextMeshProUGUI currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+        StartStopWatch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopwatchActive == true)
+        {
+            currentTime += Time.deltaTime;
+        }
+        // check System time and increment the variable time - displays minutes and seconds
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.Minutes.ToString("00") + ":" + time.Seconds.ToString("00");
+    }
+
+    public void StartStopWatch()
+    {
+        stopwatchActive = true;
+    }
+
+    public void StopStopwatch()
+    {
+        stopwatchActive = false;
+    }
+}
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     [SerializeField] int coinCount = 0;
+    [SerializeField] UIManager uIManager;
 
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(this.name + " collided with " + other.name);
         coinCount++;
+        uIManager.SetScoreCount(coinCount);
+
         Debug.Log("CoinCount: " + coinCount);
     }
 }
